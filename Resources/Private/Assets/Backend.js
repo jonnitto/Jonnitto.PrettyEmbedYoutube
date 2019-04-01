e41f0772fef9f720571758c30413ceb6c2c0c20d
Jon Uhlmann <me@jonnitto.ch>
2019-04-01 20:47:06 +0200

    :bug: Fix v4 for the new UI

--- Resources/Private/Assets/Backend.js
+++ Resources/Private/Assets/Backend.js
@@ -1,18 +1,37 @@
 import "./Preview";
 
 function NeosEvent(events, callback) {
-    events.forEach(Event => {
-        document.addEventListener("Neos." + Event, callback, false);
+    events.forEach(event => {
+        document.addEventListener("Neos." + event, callback, false);
     });
 }
 
-NeosEvent(["NodeCreated", "NodeSelected"], event => {
-    if (
-        event.detail.element.attributes["data-node-_node-type"]["value"] ==
-        "Jonnitto.PrettyEmbedYoutube:YouTube"
-    ) {
-        prettyEmbedYoutubeFixPreview();
-    }
-});
+function backendFixPreview(event) {
+    const NODE_TYPE = "Jonnitto.PrettyEmbedYoutube:YouTube";
+    try {
+        // Old UI
+        if (
+            event.detail.element.attributes["data-node-_node-type"]["value"] ==
+            NODE_TYPE
+        ) {
+            prettyEmbedYoutubeFixPreview();
+        }
+    } catch (error) {}
 
-NeosEvent(["PageLoaded", "ContentModuleLoaded"], prettyEmbedYoutubeFixPreview);
+    try {
+        // New UI
+        const node = event.detail.node;
+        if (
+            (typeof node.get == "function" &&
+                node.get("nodeType") === NODE_TYPE) ||
+            (typeof node.nodeType == "string" && node.nodeType === NODE_TYPE)
+        ) {
+            prettyEmbedYoutubeFixPreview();
+        }
+    } catch (error) {}
+}
+
+NeosEvent(["NodeCreated", "NodeSelected"], backendFixPreview);
+NeosEvent(["PageLoaded", "ContentModuleLoaded"], () => {
+    prettyEmbedYoutubeFixPreview();
+});
